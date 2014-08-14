67d6c53e72afba5fc4e42e19234d8b4bb247b194
Adam Barnhard <admbarn@gmail.com>
2014-08-14 18:43:10 -0500

    updated model

--- app/models/gambler.js
+++ app/models/gambler.js
@@ -3,35 +3,46 @@
 var _     = require('lodash'),
     Mongo = require('mongodb');
 
-function Gambler(){
+function Gambler(o){
+  o = strip(o);
+  o.spouse = strip(o.spouse);
+  this.name = o.name || 'Taker of Risks';
+  this.photo = o.photo || 'http://i.dailymail.co.uk/i/pix/2008/03_04/chimpDM2503_468x452.jpg';
+  this.cash = o.cash * 1;
+  this.spouse = {};
+  this.spouse.name = o.spouse.name || 'Lady Luck';
+  this.spouse.photo = o.spouse.photo || 'http://armedforcesmuseum.com/wp-content/uploads/2011/10/lady-Luck-Green1.jpg';
+  this.assets = [];
+  this.results = {wins:0, losses:0};
 }
 
 Object.defineProperty(Gambler, 'collection', {
   get: function(){return global.mongodb.collection('gamblers');}
 });
 
-Gambler.prototype.removeAsset = function(name, cb){
-  if(this.assets.length === 0){
-    if(cb){
-      cb({isDivorced:true, cash:this.cash});
-    }
-    return;
-  }
+Object.defineProperty(Gambler.prototype, 'isDivorced', {
+  get: function(){return this.assets.length === 0;}
+});
+
+Gambler.prototype.save = function(cb){
+  Gambler.collection.save(this, cb);
+};
+
+Gambler.prototype.sellAsset = function(name){
+  if(!this.assets.length){return;}
 
   var index = _.findIndex(this.assets, function(asset){return asset.name === name;}),
-      sold  = this.assets.splice(index, 1)[0],
-      self = this;
+      sold  = this.assets.splice(index, 1)[0];
 
   this.cash += sold.value;
   this.isDivorced = this.assets.length === 0;
-  //console.log(this);
-  Gambler.collection.save(this, function(){
-    cb({isDivorced:self.isDivorced, cash:self.cash});
-  });
 };
 
 Gambler.all = function(cb){
-  Gambler.collection.find().toArray(cb);
+  Gambler.collection.find().toArray(function(err, objs){
+    var gamblers = objs.map(function(o){return _.create(Gambler.prototype, 0);});
+    cb(gamblers);
+  });
 };
 
 Gambler.findById = function(id, cb){
@@ -43,3 +54,15 @@ Gambler.findById = function(id, cb){
 
 module.exports = Gambler;
 
+
+// helper functions
+
+function strip(o){
+  var properties = Object.keys(o);
+  properties.forEach(function(property){
+    if(typeof o[property] === 'string'){
+      o[property] = o[property].trim();
+    }
+  });
+  return o;
+}
--- test/unit/gambler.js
+++ test/unit/gambler.js
@@ -7,7 +7,9 @@ var expect    = require('chai').expect,
     Gambler   = require('../../app/models/gambler'),
     dbConnect = require('../../app/lib/mongodb'),
     cp        = require('child_process'),
-    db        = 'the-derby-test';
+    Mongo     = require('mongodb'),
+    db        = 'the-derby-test',
+    g;
 
 describe('Gambler', function(){
   before(function(done){
@@ -18,20 +20,28 @@ describe('Gambler', function(){
 
   beforeEach(function(done){
     cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      g = new Gambler({name:'Bob', photo:'bob.jpg', cash:'1000', spouse:{name:'Bobet', photo:'bobet.jpg'}});
       done();
     });
   });
 
   describe('constructor', function(){
     it('should create a new Gambler object', function(){
-      var g = new Gambler();
       expect(g).to.be.instanceof(Gambler);
+      expect(g.name).to.equal('Bob');
+      expect(g.photo).to.equal('bob.jpg');
+      expect(g.cash).to.equal(1000);
+      expect(g.spouse.name).to.equal('Bobet');
+      expect(g.spouse.photo).to.equal('bobet.jpg');
+      expect(g.assets).to.have.length(0);
+      expect(g.results.wins).to.equal(0);
+      expect(g.results.losses).to.equal(0);
     });
   });
 
   describe('.all', function(){
     it('should get all gamblers', function(done){
-      Gambler.all(function(err, gamblers){
+      Gambler.all(function(gamblers){
         expect(gamblers).to.have.length(3);
         done();
       });
@@ -42,31 +52,47 @@ describe('Gambler', function(){
     it('should find a gambler in database', function(done){
       Gambler.findById('000000000000000000000001', function(gambler){
         expect(gambler._id.toString()).to.equal('000000000000000000000001');
-        expect(gambler).to.respondTo('removeAsset');
+        expect(gambler).to.respondTo('sellAsset');
         done();
       });
     });
   });
 
-  describe('#removeAsset', function(){
-    it('should remove an asset from the gambler and increase cash', function(done){
-      Gambler.findById('000000000000000000000001', function(gambler){
-        gambler.removeAsset('Ring', function(obj){
-          expect(obj.cash).to.be.closeTo(5000, 0.1);
-          expect(obj.isDivorced).to.be.false;
-          expect(gambler.assets).to.have.length(2);
-          done();
-        });
+  describe('#save', function(){
+    it('should save a gambler in the database', function(done){
+      g.save(function(){
+        expect(g._id).to.be.instanceof(Mongo.ObjectID);
+        done();
       });
     });
-    it('should not remove an asset from the gambler or increase cash', function(done){
+  });
+
+  describe('#isDivorced', function(){
+    it('should return false if 1 or more assests left', function(){
+      g.assets = [{name:'test', photo:'test.jpg', value:5}];
+      expect(g.isDivorced).to.equal(false);
+    });
+    it('should return true if 0 assests left', function(){
+      g.assets = [];
+      expect(g.isDivorced).to.equal(true);
+    });
+  });
+
+  describe('#sellAsset', function(){
+    it('should remove an asset from the gambler and increase cash', function(){
+      Gambler.findById('000000000000000000000001', function(gambler){
+        gambler.sellAsset('Ring');
+        expect(gambler.cash).to.be.closeTo(5000, 0.1);
+        expect(gambler.isDivorced).to.equal(false);
+        expect(gambler.assets).to.have.length(2);
+      });
+    });
+    it('should not remove an asset from the gambler or increase cash', function(){
       Gambler.findById('000000000000000000000002', function(gambler){
         gambler.assets = [];
-        gambler.removeAsset('Ring', function(obj){
-          expect(obj.cash).to.be.closeTo(1000, 0.1);
-          expect(gambler.assets).to.have.length(0);
-          done();
-        });
+        gambler.sellAsset('Ring');
+        expect(gambler.cash).to.be.closeTo(1000, 0.1);
+        expect(gambler.assets).to.have.length(0);
       });
     });
   });
